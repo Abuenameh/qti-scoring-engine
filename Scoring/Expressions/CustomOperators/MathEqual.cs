e549c295c8ce3e195de8aa35270971707e73c247
Abuenameh <abuenameh@gmail.com>
2024-04-25 14:20:02 -0400

    Fix formatting

--- Scoring/Expressions/CustomOperators/MathEqual.cs
+++ Scoring/Expressions/CustomOperators/MathEqual.cs
@@ -14,7 +14,8 @@ namespace Citolab.QTI.ScoringEngine.ResponseProcessing.CustomOperators
     {
         public BaseValue Apply(List<BaseValue> values)
         {
-            if (values.Count == 0) {
+            if (values.Count == 0)
+            {
                 return new BaseValue
                 {
                     BaseType = BaseType.String,
@@ -24,12 +25,14 @@ namespace Citolab.QTI.ScoringEngine.ResponseProcessing.CustomOperators
 
             var engine = ScoringEngine.GetEngine();
             engine.Execute("var ce = new ComputeEngine.ComputeEngine()");
-            if (values[0].Identifier?.Equals("setup") ?? false) {
+            if (values[0].Identifier?.Equals("setup") ?? false)
+            {
                 engine.Execute(values[0].Value);
                 values.RemoveAt(0);
             }
 
-            if (values.Count < 2) {
+            if (values.Count < 2)
+            {
                 return new BaseValue
                 {
                     BaseType = BaseType.String,
