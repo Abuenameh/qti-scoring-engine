37b95d6eddab869ece43b3fa10f7ad12c55092cf
Abuenameh <abuenameh@gmail.com>
2024-04-25 11:08:09 -0400

    Make setup argument optional

--- Scoring/Expressions/CustomOperators/MathEqual.cs
+++ Scoring/Expressions/CustomOperators/MathEqual.cs
@@ -14,26 +14,35 @@ namespace Citolab.QTI.ScoringEngine.ResponseProcessing.CustomOperators
     {
         public BaseValue Apply(List<BaseValue> values)
         {
-            var numValues = values.Count;
-            if (numValues < 3) {
+            if (values.Count == 0) {
                 return new BaseValue
                 {
-                    BaseType = BaseType.Boolean,
+                    BaseType = BaseType.String,
                     Value = "False"
                 };
             }
 
-            var setup = values[0].Value;
             var engine = ScoringEngine.GetEngine();
             engine.Execute("var ce = new ComputeEngine.ComputeEngine()");
-            engine.Execute(setup);
+            if (values[0].Identifier?.Equals("setup") ?? false) {
+                engine.Execute(values[0].Value);
+                values.RemoveAt(0);
+            }
 
-            var lhs = values[1].Value.Replace("\\", "\\\\");
-            var equal = values.ToList().Skip(2).Select(rhs => engine.Evaluate($"ce.parse('{lhs}').isEqual(ce.parse('{rhs.Value.Replace("\\", "\\\\")}'))")).Any(x => x.AsBoolean());
+            if (values.Count < 2) {
+                return new BaseValue
+                {
+                    BaseType = BaseType.String,
+                    Value = "False"
+                };
+            }
+
+            var lhs = values[0].Value.Replace("\\", "\\\\");
+            var equal = values.ToList().Skip(1).Select(rhs => engine.Evaluate($"ce.parse('{lhs}').isEqual(ce.parse('{rhs.Value.Replace("\\", "\\\\")}'))")).Any(x => x.AsBoolean());
 
             return new BaseValue
             {
-                BaseType = BaseType.Boolean,
+                BaseType = BaseType.String,
                 Value = equal.ToString()
             };
         }
